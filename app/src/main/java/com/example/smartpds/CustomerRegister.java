717310f2ce11c8563d50c7aeb68efdcfe05cef04
raj-patil <techbelievers@gmail.com>
2020-07-31 18:35:31 +0530

    QR

--- app/src/main/java/com/example/smartpds/CustomerRegister.java
+++ app/src/main/java/com/example/smartpds/CustomerRegister.java
@@ -28,11 +28,8 @@ import com.androidnetworking.interfaces.DownloadListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -98,21 +95,21 @@ String name;
         customer.setState(customerState.getText().toString().trim());
         customer.setPincode(Integer.parseInt(customerPincode.getText().toString().trim()));
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 databaseReference.child(mobile).setValue(customer);
                 generateDistributorQR();
                 Toast.makeText(CustomerRegister.this, "Added into Customers", Toast.LENGTH_SHORT).show();
                 Intent intent=new Intent(CustomerRegister.this,CustomerKycRegister.class);
                 intent.putExtra("mobile",mobile);
                 startActivity(intent);
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+//            }
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
     }
     private void generateDistributorQR() {
         while(count!=1) {
